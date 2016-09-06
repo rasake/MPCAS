ffcbbaeecb0567b59930e0d3da2a048edc2a7aff
rasake <rasake@student.chalmers.se>
2016-09-06 15:20:23 +0200

    Fix bug by initializing weights to zeros, not ones, and adding cloning for all setters to avoid unintended mutations

--- FFR135 Artificial Neural Networks/Assignment 1/hopfield.py
+++ FFR135 Artificial Neural Networks/Assignment 1/hopfield.py
@@ -11,7 +11,7 @@ class HopfieldNetwork:
         if initial_pattern is None:
             initial_pattern = np.ones([nbr_of_cells, 1])
         if initial_weights is None:
-            initial_weights = np.ones([nbr_of_cells, nbr_of_cells])
+            initial_weights = np.zeros([nbr_of_cells, nbr_of_cells])
         self._NBR_OF_CELLS = nbr_of_cells
         self.neuron_state_vector = initial_pattern
         self.weights = initial_weights
@@ -25,7 +25,7 @@ class HopfieldNetwork:
     def neuron_state_vector(self, pattern_vector):
         if len(pattern_vector) != self._NBR_OF_CELLS:
             raise ValueError("Size of Neural Network cannot change")
-        self._neuron_state_vector = pattern_vector
+        self._neuron_state_vector = np.copy(pattern_vector)
 
     @property
     def weights(self):
@@ -35,11 +35,12 @@ class HopfieldNetwork:
     def weights(self, new_weights):
         if np.shape(new_weights) != (self._NBR_OF_CELLS, self._NBR_OF_CELLS):
             raise ValueError("Weight matrix cannot change size")
-        self._weights = np.copy(new_weights)                
+        self._weights = np.copy(new_weights)   
 
     
     def feed_pattern(self, pattern_vector):
-        self.neuron_state_vector = pattern_vector
+        self.neuron_state_vector = np.copy(pattern_vector)
+        self._updates_since_last_reset = 0
 
                
     def store_pattern(self, pattern_vector):
@@ -47,11 +48,11 @@ class HopfieldNetwork:
             raise ValueError("Pattern length must match number of neurons")
         temp_weights = pattern_vector @ np.transpose(pattern_vector) / self._NBR_OF_CELLS
         np.fill_diagonal(temp_weights,0)       
-        self._weights += temp_weights
+        self.weights += temp_weights
 
     def update_state(self):
         new_state = np.sign(self._weights @ self._neuron_state_vector)
         is_done = np.array_equal(self.neuron_state_vector, new_state)
-        self.neuron_state_vector = np.copy(new_state)
+        self.neuron_state_vector = new_state
         self._updates_since_last_reset +=1
         return is_done
