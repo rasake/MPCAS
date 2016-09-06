f2b55398ef834e69621264943c3a293ae259e36d
rasake <rasake@student.chalmers.se>
2016-09-06 15:22:33 +0200

    Fix main file, it now generates simulated probabilities approximately equals to therortical values

--- FFR135 Artificial Neural Networks/Assignment 1/main.py
+++ FFR135 Artificial Neural Networks/Assignment 1/main.py
@@ -5,6 +5,8 @@ Created on Mon Sep  5 17:13:19 2016
 @author: Rasmus
 """
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 import hopfield as hf
 
 
@@ -18,45 +20,36 @@ def store_random_patterns(hopfield_network, nbr_of_patterns):
         hopfield_network.store_pattern(pattern_i)
     return patterns
 
-def simulate_on_step_error_prob(pattern_length, nbr_of_patterns):
+def simulate_one_step_error_prob(pattern_length, nbr_of_patterns):
     hopfield_network = hf.HopfieldNetwork(pattern_length)
     stored_patterns = store_random_patterns(hopfield_network, nbr_of_patterns)
-    nbr_of_tests = int(600 / pattern_length)
-    temp_patterns = [stored_patterns[np.random.randint(nbr_of_tests)] for x in range(nbr_of_tests)]
     one_step_errors = 0
-    for pattern_i in temp_patterns:
+    for pattern_i in stored_patterns:
         hopfield_network.feed_pattern(pattern_i)
         hopfield_network.update_state()
-        print(sum(abs(pattern_i - hopfield_network.neuron_state_vector)))
-        one_step_errors += np.sum(abs(pattern_i - hopfield_network.neuron_state_vector))
-    print(one_step_errors)
-    print(hopfield_network._NBR_OF_CELLS)
-    return one_step_errors/ (nbr_of_tests * pattern_length)
+        one_step_errors += np.sum(abs(pattern_i - hopfield_network.neuron_state_vector)) / 2 # Dvision by two since bits are either 1 or -1 so the difference is 2 or -2 if a bit flips
+    return one_step_errors/ (len(stored_patterns) * hopfield_network._NBR_OF_CELLS)
 
+
+def simulate_prob_curve():
+    N = [100, 200]
+    P = [10, 20, 30, 40, 50, 75, 100, 150, 200]
+    P_over_N = np.zeros(len(N)*len(P))
+    probabilities = np.zeros(len(N)*len(P))
+    for i, N_i in enumerate(N):
+        for j, P_i in enumerate(P):
+            P_over_N[i+j] = P_i / N_i
+            probabilities[i+j] = simulate_one_step_error_prob(N_i,P_i)
+    return (P_over_N, probabilities)
+
+def make_prob_comp_plot():
+    x = np.linspace(0.001,1)
+    analytical_prob = [0.5*(1-math.erf(math.sqrt(1/x_i/2))) for x_i in x]
+    simulated_curve = simulate_prob_curve()
+    plt.plot(simulated_curve[0], simulated_curve[1], 'rx')
+    plt.plot(x, analytical_prob)    
+    plt.show()
     
-print(simulate_on_step_error_prob(100,20))
 
+make_prob_comp_plot()
     
-# randomly select a few of the stored patterns, feed them to the network one at a time, check succesrate for each after on step
-    
-    
-"""
-
-s = np.array([[-1],[-1]])
-W = np.array([[0,1],[0,1]])
-u = np.array([[-1],[-1]])
-
-
-Albert = hf.HopfieldNetwork(2)
-
-Albert.feed_pattern(s)
-
-Albert.store_pattern(u)
-Albert.store_pattern(u)
-
-print(Albert.weights)
-
-print(Albert.neuron_state_vector)
-print(Albert.update_state())
-print(Albert.neuron_state_vector)
-"""
