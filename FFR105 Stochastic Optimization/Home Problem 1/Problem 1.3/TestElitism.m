fa2e9a1bc51513bc76ab4246ffa7b4306809d7f5
rasake <rasake@student.chalmers.se>
2016-09-14 14:39:59 +0200

    Add function InsertBestIndividual for elitism

---
+++ FFR105 Stochastic Optimization/Home Problem 1/Problem 1.3/TestElitism.m
@@ -0,0 +1,10 @@
+
+
+population = InitializePopulation( 3, 5 )
+
+aIndividual = population(2,:)
+modified_population = InsertBestIndividual(population,aIndividual,1)
+
+modified_population = InsertBestIndividual(population,aIndividual,2)
+
+modified_population = InsertBestIndividual(population,aIndividual,3)
