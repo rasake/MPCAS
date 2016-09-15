3099b45df72d56a7ad901a7a24d41620fc505d6c
rasake <rasake@student.chalmers.se>
2016-09-15 09:51:24 +0200

    Tournament selectcion implemented

--- FFR105 Stochastic Optimization/Home Problem 1/Problem 1.3/TournamentSelect.m
+++ FFR105 Stochastic Optimization/Home Problem 1/Problem 1.3/TournamentSelect.m
@@ -1,24 +1,36 @@
-function [ iSelected ] = TournamentSelect( fitness, tournamentSelectionParameter )
+function [ iSelected ] = TournamentSelect( fitness, tournamentSelectionParameter, tournamentSize )
 
-populationSize = size(fitness,1);
-iTmp1 = 1 + fix(rand*populationSize);
-iTmp2 = 1 + fix(rand*populationSize);
+populationSize = size(fitness,2);
+indicesContestants = zeros(1,tournamentSize);
+fitnessContestants = zeros(1,tournamentSize);
+% Randomly Select individuals to enter the tournament
+for k = 1:tournamentSize
+    kIndex = 1 + fix(rand*populationSize);
+    kFitness = fitness(kIndex);
+    indicesContestants(k) = kIndex;
+    fitnessContestants(k) = kFitness;
+end
 
+iSelected = tournamentHeat(fitnessContestants, indicesContestants, tournamentSelectionParameter);
+
+end
+
+
+function iSelected = tournamentHeat(fitnessContestants, indicesContestants, tournamentSelectionParameter)
 r = rand;
-
-if (r < tournamentSelectionParameter)
-   if (fitness(iTmp1) > fitness(iTmp2))
-       iSelected = iTmp1;
-   else
-        iSelected = iTmp2;
-   end
-else
-   if(fitness(iTmp1) > fitness(iTmp2))
-       iSelected = iTmp2;
-   else
-        iSelected = iTmp1;
-   end
+if r < tournamentSelectionParameter % return best individual
+    [fitnessBest, localIndicesBestIndivuals] = max(fitnessContestants); % Note that there may be more than one best indivudal
+    iBestIndividual = indicesContestants(localIndicesBestIndivuals(1)); % In the case of multiple best individuals we choose the first one
+    iSelected = iBestIndividual;
+elseif length(fitnessContestants) == 1 % return worst individual
+    iSelected = indicesContestants;
+else % Remove best individual from tournament and have anoher heat
+    [fitnessBest, indexTmp] = max(fitnessContestants); % Note that there may be more than one best indivudal
+    localIndexBestIndivual = indexTmp(1);  % In the case of multiple best individuals we choose the first one
+    fitnessContestants = [fitnessContestants(1:localIndexBestIndivual-1) fitnessContestants(localIndexBestIndivual+1:end)];
+    indicesContestants = [indicesContestants(1:localIndexBestIndivual-1) indicesContestants(localIndexBestIndivual+1:end)];
+    iSelected = tournamentHeat(fitnessContestants, indicesContestants, tournamentSelectionParameter);
 end
-end
-   
 
+
+end
