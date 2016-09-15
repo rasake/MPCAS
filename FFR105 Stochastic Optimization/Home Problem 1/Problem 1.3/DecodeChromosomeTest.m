5513e942b029a2bc51dd30c1281ae09a7e2061e5
rasake <rasake@student.chalmers.se>
2016-09-15 10:28:21 +0200

    Expand testing for DecodeChromosome

--- FFR105 Stochastic Optimization/Home Problem 1/Problem 1.3/DecodeChromosomeTest.m
+++ FFR105 Stochastic Optimization/Home Problem 1/Problem 1.3/DecodeChromosomeTest.m
@@ -23,3 +23,22 @@ xExpected = [-0.8710, -0.8710, -0.8710];
 xCalculated = DecodeChromosome(chromosome, 3, variableRange);
 error = xExpected - xCalculated;
 assert(norm(error) < 0.0001)
+
+
+%% Test 4: Different variable values
+chromosome = [0 1 0 1 1 0 1 0 1 1 1 0 0 0 0 0 0 0 0 0];
+variableRange = 3;
+xExpected = [-0.8710, 0.0029];
+xCalculated = DecodeChromosome(chromosome, 2, variableRange);
+error = xExpected - xCalculated;
+assert(norm(error) < 0.0001)
+
+
+
+%% Test 5: VariableRange [-5,5]
+chromosome = [1 1 0 0 0 1];
+variableRange = 5;
+xExpected = [3.5714, -3.5714];
+xCalculated = DecodeChromosome(chromosome, 2, variableRange);
+error = xExpected - xCalculated;
+assert(norm(error) < 0.0001)
