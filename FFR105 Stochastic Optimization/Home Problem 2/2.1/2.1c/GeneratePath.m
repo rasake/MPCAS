1f309739ac69d8955c343ed0a925009635dc472c
rasake <rasake@student.chalmers.se>
2016-10-04 17:48:03 +0200

    Workin function for finding nearest-neighbour path length

---
+++ FFR105 Stochastic Optimization/Home Problem 2/2.1/2.1c/GeneratePath.m
@@ -0,0 +1,7 @@
+function [ output_args ] = GeneratePath(pheromoneLevel, visibility, alpha, beta)
+%GENERATEPATH Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ FFR105 Stochastic Optimization/Home Problem 2/2.1/2.1c/InitializePheromoneLevels.m
@@ -0,0 +1,7 @@
+function [ output_args ] = InitializePheromoneLevels(numberOfCities, tau0)
+%INITIALIZEPHEROMONELEVELS Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
