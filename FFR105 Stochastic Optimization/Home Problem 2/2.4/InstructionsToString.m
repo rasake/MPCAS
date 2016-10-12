8480ba6e9bc3c8097436245250e6a0c2b69f1cb0
rasake <rasake@student.chalmers.se>
2016-10-12 17:22:45 +0200

    Minor fixes for instruction decoding

--- FFR105 Stochastic Optimization/Home Problem 2/2.4/InstructionsToString.m
+++ FFR105 Stochastic Optimization/Home Problem 2/2.4/InstructionsToString.m
@@ -17,31 +17,58 @@ end
 
 
 for i = 0:nbrOfInstructions-1
+%     disp('----------')
     operator = chromosome(4*i+1);
     destinationIndex = chromosome(4*i+2);
     operand1 = num2str(masterRegisters{chromosome(4*i+3)});
     operand2 = num2str(masterRegisters{chromosome(4*i+4)});
+%     disp( [num2str(operator) ' '  num2str(destinationIndex) ' ' num2str(operand1) ' ' num2str(operand2)])
     switch operator
         case 1
-            result = [operand1 '+' operand2];
+            if operand1 == '0' & isempty(strfind(operand2,'Inf'))
+                result = operand2;
+            elseif operand2 == '0' & isempty(strfind(operand1,'Inf'))
+                result = operand1;
+            else
+                result = [operand1 '+' operand2];
+            end
         case 2
-            if operand1 == operand2
+            if length(operand1) == length(operand2) & operand1 == operand2
                 result = '0';
             else
                 result = [operand1 '-' operand2];
             end
         case 3
-            result = [operand1 '*' operand2];
+            if operand1 == '1'
+                result = operand2;
+            elseif operand2 == '1'
+                result = operand1;
+            elseif length(operand1)==2 & length(operand2)==2 & operand1=='-1' & operand2=='-1'
+                result = '1';
+            elseif length(operand1) == 2 & operand1 == '-1'
+                result = ['(-' operand2 ')'];
+            elseif length(operand2) == 2 & operand2 == '-1'
+                result = ['-(' operand1 ')'];
+            elseif length(operand1) == length(operand2) & operand1 == operand2
+                if operand1(1) == '-'
+                    result = ['(' operand1(2:end) ')^2'];
+                else
+                    result = ['(' operand1 ')^2'];  
+                end          
+            else
+                result = ['(' operand1 ')*(' operand2 ')'];
+            end            
         case 4
             if operand2 == '0'
                 result = 'Inf';
-            elseif operand1 == operand2
+            elseif length(operand1) == length(operand2) & operand1 == operand2
                 result = '1';
             else
                 result = ['(' operand1 ')/(' operand2 ')'];
             end
     end
     masterRegisters{destinationIndex} = result;
+%     disp('----------')
 
 y = masterRegisters{1};
 
--- FFR105 Stochastic Optimization/Home Problem 2/2.4/TestFit.m
+++ FFR105 Stochastic Optimization/Home Problem 2/2.4/TestFit.m
@@ -1,20 +1,33 @@
+close all
+%bestChromosomeEver = [1  4  4  4  1  1  1  2  2  2  2  2  1  4  2  1  1  2  6  6  3  2  1  4  3  3  2  2  2  3  2  6  4  1  1  3  1  3  1  6];
+% ganska nära
+%bestChromosomeEver = [2  3  1  7  2  5  2  5  3  1  3  6  3  4  3  5  1  5  2  1  4  5  1  6  4  3  7  3  4  1  4  6  1  1  3  6  3  2  6  5  2  2  5  1  4  1  6  2  2  5  6  4  2  4  3  7];
+bestChromosomeEver = [1  3  1  3  2  2  3  7  2  4  4  5  1  5  2  2  1  5  4  7  4  3  7  4  3  3  2  7  3  4  2  7  3  2  3  6  3  3  2  3  2  1  7  3  1  4  5  7  4  1  2  1  3  2  2  7];
 
-bestChromosomeEver = [1  4  2  6  1  1  2  6  1  3  6  6  1  3  6  2  3  2  2  6  2  1  5  5  1  3  2  3  1  2  6  4  3  2  3  2];
-nbrVarRegisters = 4;
+
+nbrVarRegisters = 5;
 constants = [1 -1];
 
 functionData = LoadFunctionData;
 nbrDataPoints = size(functionData,1);
-
+x = functionData(:,1);
 resultingFunction = InstructionsToString(bestChromosomeEver, nbrVarRegisters, constants);
-disp('Best function found')
+disp('Best function can be simplified to')
 disp(['y = ' resultingFunction])
 y_hat = zeros(1,nbrDataPoints);
 for i = 1:nbrDataPoints
-    y_hat(i) = RunInstructions(functionData(i,1), bestChromosomeEver, nbrVarRegisters, constants);    
+    y_hat(i) = RunInstructions(x(i), bestChromosomeEver, nbrVarRegisters, constants);    
 end
+
+y_hat2 = zeros(1,nbrDataPoints);
+for i = 1:nbrDataPoints
+    y_hat2(i) =  (-(x(i)--1))/(-1-(-(x(i)--1))^2);
+end
+
+
 
 hold on
 plot(functionData(:,1), functionData(:,2))
-plot(functionData(:,1), y_hat, 'r*')
+plot(functionData(:,1), y_hat, 'r')
+plot(functionData(:,1), y_hat2, 'g*')
 
