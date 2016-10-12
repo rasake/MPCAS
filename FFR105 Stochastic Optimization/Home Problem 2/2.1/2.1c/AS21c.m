14a5708d473e4f320db5a5f73b70cae705485598
rasake <rasake@student.chalmers.se>
2016-10-12 17:58:39 +0200

    Rename main scripts to foolow assignment instructions

---
+++ FFR105 Stochastic Optimization/Home Problem 2/2.1/2.1c/AS21c.m
@@ -0,0 +1,81 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Ant system (AS) for TSP.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clf;
+clear all;
+clc;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Data
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+cityLocation = LoadCityLocations();
+numberOfCities = length(cityLocation);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+numberOfAnts = numberOfCities;  % To do: Set to appropriate value.
+alpha = 1.0;        % To do: Set to appropriate value.
+beta = 3.0;         % To do: Set to appropriate value.
+rho = 0.5;          % To do: set to appropriate value.
+
+% To do: Write the GetNearestNeighbourPathLength function
+nearestNeighbourPathLength = GetNearestNeighbourPathLength(cityLocation);
+tau0 = numberOfAnts/nearestNeighbourPathLength;
+
+targetPathLength = 123.0;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Initialization
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+range = [0 20 0 20];
+tspFigure = InitializeTspPlot(cityLocation, range);
+connection = InitializeConnections(cityLocation);
+pheromoneLevel = InitializePheromoneLevels(numberOfCities, tau0);
+visibility = GetVisibility(cityLocation);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Main loop
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+minimumPathLength = inf;
+
+iIteration = 0;
+
+while (minimumPathLength > targetPathLength)
+ iIteration = iIteration + 1;
+
+ %%%%%%%%%%%%%%%%%%%%%%%%%%
+ % Generate paths:
+ %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ pathCollection = [];
+ pathLengthCollection = [];
+ for k = 1:numberOfAnts
+  path = GeneratePath(pheromoneLevel, visibility, alpha, beta);
+  pathLength = GetPathLength(path,cityLocation);
+  if (pathLength < minimumPathLength)
+    minimumPathLength = pathLength;
+    disp(sprintf('Iteration %d, ant %d: path length = %.5f',iIteration,k,minimumPathLength));
+    PlotPath(connection,cityLocation,path);
+  end
+  pathCollection = [pathCollection; path];           
+  pathLengthCollection = [pathLengthCollection; pathLength];
+ end
+
+ %%%%%%%%%%%%%%%%%%%%%%%%%%
+ % Update pheromone levels
+ %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ deltaPheromoneLevel = ComputeDeltaPheromoneLevels(pathCollection,pathLengthCollection);  % To do: write the ComputeDeltaPheromoneLevels function
+ pheromoneLevel = UpdatePheromoneLevels(pheromoneLevel,deltaPheromoneLevel,rho);          % To do: write the UpdatePheromoneLevels function
+
+end
+
+
+
+
+
+
