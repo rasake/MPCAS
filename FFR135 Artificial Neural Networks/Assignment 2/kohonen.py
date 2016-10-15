c709664dd83330d8e4b1641384de79b3f315d88b
rasake <rasake@student.chalmers.se>
2016-10-15 14:57:36 +0200

    Code for task1

---
+++ FFR135 Artificial Neural Networks/Assignment 2/kohonen.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 15 14:51:42 2016
+
+@author: Rasmus
+"""
+import numpy as np
+from numpy.linalg import norm
+
+def neighbourhood_function(positions, winning_neuron_index, sigma):
+    win_pos = positions[winning_neuron_index]
+    return [np.exp(- norm(pos_i-win_pos)**2/(2*sigma**2) ) for pos_i in positions]
+
+def fold_neurons(nbr_neurons, nbr_folds):
+    if nbr_folds == 0:
+        raise ValueError('There has to be at least one fold')
+    return [np.array([index//nbr_folds, index%nbr_folds]) for index in range(nbr_neurons)]
+
+def find_winning_neuron(weight_matrix, pattern):
+    euclidian_dist = [norm(np.reshape(w_i,[2,1])-pattern) for w_i in weight_matrix]
+    return np.argmin(euclidian_dist)
+
+
+def update_weight_matrix(weight_matrix, positions, sample, sigma, eta):
+     #Updates weight matrix IN PLACE (!)
+     winning_neuron = find_winning_neuron(weight_matrix, sample)
+     proximity_factors = neighbourhood_function(positions, winning_neuron, sigma)
+     delta_w = [proximity_factors[i]*(np.reshape(sample, [2]) - w_i) for i,w_i in enumerate(weight_matrix)]
+     weight_matrix += eta * np.reshape(delta_w, [len(delta_w),2])
