d03569b6edad032c40eda48cb6887e50b22b98c0
rasake <rasake@student.chalmers.se>
2017-11-05 10:24:34 +0100

    Move some functions to separate script

--- MVE190 Linear Statistical Models/Seminar1/housing_main.py
+++ MVE190 Linear Statistical Models/Seminar1/housing_main.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 from scipy import stats
+import regutil
 
 
 
@@ -58,46 +59,7 @@ def analyze_basement(housing_data):
     plt.savefig(os.path.join('figs', 'log_'+ x_label + '_vs_log_' + y_label + '.png'), dpi=700)
     plt.close()
 
-
-
-def make_regplot(x,y, x_label, y_label, save_folder):
-    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
-    ax = sns.regplot(x,y)
-    # these are matplotlib.patch.Patch properties
-    props = dict(boxstyle='round', facecolor='wheat', alpha=0.8)
-    textstr = '$\mathrm{intercept} = %.3f$\n$\mathrm{slope}=%.2f$\n$\mathrm{r2}=%.2f$'%(intercept, slope, r_value**2)
-    # place a text box in upper left in axes coords
-    ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
-        verticalalignment='top', bbox=props)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.savefig(os.path.join(save_folder, x_label + '_vs_'
-        + y_label + '.png'), dpi=700)
-    plt.close()
-    return (slope, intercept)
-
-
-def make_error_plots(x, y, e, x_label, y_label, save_folder):
-    plt.scatter(x,e)
-    plt.xlabel(x_label)
-    plt.ylabel('Residual')
-    plt.savefig(os.path.join(save_folder, 'Xerrorplot_' + x_label + '_vs_'
-        + y_label + '.png'), dpi = 700)
-    plt.close()    
-
-    plt.scatter(y,e)
-    plt.xlabel(y_label)
-    plt.ylabel('Residual')
-    plt.savefig(os.path.join(save_folder, 'Yerrorplot_' + x_label + '_vs_'
-        + y_label + '.png'), dpi = 700)
-    plt.close()
-
-def make_plots(x, y, x_label, y_label, save_folder):
-    slope, intercept = make_regplot(x, y, x_label, y_label, save_folder)    
-    yhat = [slope*xi + intercept for xi in x]
-    e = yhat-y  
-    make_error_plots(x, y, e, x_label, y_label, save_folder)
-        
+       
 
 def set_up(housing_data, x_label, y_label):
     save_folder = os.path.join('figs', x_label)
@@ -114,12 +76,12 @@ def analyze_grade(housing_data):
     (x, y, save_folder) = set_up(housing_data, x_label, y_label)
 
     # without tranformation
-    make_plots(x, y, x_label, y_label, save_folder)
+    regutil.make_plots(x, y, x_label, y_label, save_folder)
 
     # transform
     y = np.log(y)
     y_label = 'Log(' + y_label + ')'
-    make_plots(x, y, x_label, y_label, save_folder)
+    regutil.make_plots(x, y, x_label, y_label, save_folder)
 
                                                                                                                                                                                                                                                                                                                                                                                                                                             
 
@@ -129,14 +91,14 @@ def analyze_living(housing_data):
     (x, y, save_folder) = set_up(housing_data, x_label, y_label)
 
     # without tranformation
-    make_plots(x, y, x_label, y_label, save_folder)
+    regutil.make_plots(x, y, x_label, y_label, save_folder)
 
     # transform    
     x = np.log(x)
     y = np.log(y)
     x_label = 'Log(' + x_label + ')'
     y_label = 'Log(' + y_label + ')'
-    make_plots(x, y, x_label, y_label, save_folder)
+    regutil.make_plots(x, y, x_label, y_label, save_folder)
     
 def analyze_bathrooms(housing_data):
     x_label = 'bathrooms'
@@ -144,12 +106,12 @@ def analyze_bathrooms(housing_data):
     (x, y, save_folder) = set_up(housing_data, x_label, y_label)
 
     # without tranformation
-    make_plots(x, y, x_label, y_label, save_folder)
+    regutil.make_plots(x, y, x_label, y_label, save_folder)
 
     # transform
     y = np.log(y)
     y_label = 'Log(' + y_label + ')'
-    make_plots(x, y, x_label, y_label, save_folder)
+    regutil.make_plots(x, y, x_label, y_label, save_folder)
 
 
 def main():
